ea56870b60a3d2ea80eb30b1fabc742187a0022d
asorichetti <107128425+asorichetti@users.noreply.github.com>
2024-08-14 12:30:42 -0400

    Created index file to begin coding utility

---
+++ index.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, this will be a GoLang Pokedex Application to serve up pokemon based on user input, this statement is temporary")
+}
